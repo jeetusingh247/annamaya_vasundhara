14f41f10834ef2c95f14adf9ae9f8d3307bd9cab
jeetusingh247 <jeetusingh11010@gmail.com>
2025-02-06 19:56:36 +0530

    signup page in progress left for image & logo  insertion

---
+++ frontend/src/pages/Signup.jsx
@@ -0,0 +1,64 @@
+import React from "react";
+
+const SignUp = () => {
+  return (
+    <div className="min-h-screen bg-gray-100 text-gray-900 flex justify-center">
+      <div className="max-w-screen-xl m-0 sm:m-10 bg-white shadow sm:rounded-lg flex justify-center flex-1">
+        <div className="lg:w-1/2 xl:w-5/12 p-6 sm:p-12">
+          <div>
+            <img
+              src="https://drive.google.com/uc?export=view&id=1MFiKAExRFF0-2YNpAZzIu1Sh52J8r16v"
+              className="mx-auto"
+              alt="Logo"
+            />
+          </div>
+          <div className="mt-12 flex flex-col items-center">
+            <div className="w-full flex-1 mt-8">
+              <div className="mx-auto max-w-xs">
+                <input
+                  className="w-full px-8 py-4 rounded-lg font-medium bg-gray-100 border border-gray-200 placeholder-gray-500 text-sm focus:outline-none focus:border-gray-400 focus:bg-white"
+                  type="text"
+                  placeholder="Full Name"
+                />
+                <input
+                  className="w-full px-8 py-4 rounded-lg font-medium bg-gray-100 border border-gray-200 placeholder-gray-500 text-sm focus:outline-none focus:border-gray-400 focus:bg-white mt-5"
+                  type="email"
+                  placeholder="Email"
+                />
+                <input
+                  className="w-full px-8 py-4 rounded-lg font-medium bg-gray-100 border border-gray-200 placeholder-gray-500 text-sm focus:outline-none focus:border-gray-400 focus:bg-white mt-5"
+                  type="password"
+                  placeholder="Password"
+                />
+                <button className="mt-5 tracking-wide font-semibold bg-green-400 text-white w-full py-4 rounded-lg hover:bg-green-700 transition-all duration-300 ease-in-out flex items-center justify-center focus:shadow-outline focus:outline-none">
+                  <span className="ml-2">Register</span>
+                </button>
+                <p className="mt-6 text-xs text-gray-600 text-center">
+                  By signing up, I agree to the
+                  <a href="#" className="border-b border-gray-500 border-dotted">
+                    Terms of Service
+                  </a>
+                  and
+                  <a href="#" className="border-b border-gray-500 border-dotted">
+                    Privacy Policy
+                  </a>
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="flex-1 bg-green-100 text-center hidden lg:flex">
+          <div
+            className="m-12 xl:m-16 w-full bg-contain bg-center bg-no-repeat"
+            style={{
+              backgroundImage:
+                "url('https://drive.google.com/uc?export=view&id=1KZ_Ub_2lZ0dHbKV0fAIhxVhiQA183RCz')",
+            }}
+          ></div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default SignUp;
