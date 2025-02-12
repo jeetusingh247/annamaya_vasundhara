e64944134057579d7196aee92d0906c2a3a4ecad
jeetusingh247 <jeetusingh11010@gmail.com>
2025-02-12 11:43:06 +0530

    modified about.jsxfor section_01 & herosection components

--- frontend/src/pages/About.jsx
+++ frontend/src/pages/About.jsx
@@ -2,17 +2,16 @@ import React from 'react';
 import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
 import HeroSection from "../components/HeroSection"; // Import the HeroSection component
-
+import SectionAbout01 from "../components/Section_About_01.jsx"; // Import the SectionAbout01 component
 const About = () => {
   return (
     <>
       <Navbar />
       <HeroSection /> {/* Include the HeroSection component */}
       <main className="container mx-auto p-5">
-        <h1 className="text-3xl font-bold">About Annamaya Vasundhara</h1>
-        <p className="mt-4 text-gray-700">
-          Our mission is to create environmental awareness and promote sustainable practices.
-        </p>
+        <div>
+        <SectionAbout01 /> {/* Include the SectionAbout01 component */}
+        </div>
       </main>
       <Footer />
     </>
