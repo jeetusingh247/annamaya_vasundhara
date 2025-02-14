05203fbd745d8fbf3c6018df0aae1a31592a481e
jeetusingh247 <jeetusingh11010@gmail.com>
2025-02-14 15:56:29 +0530

    modified navbar

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,9 +1,16 @@
+import { useState } from "react";
 import { Link } from "react-router-dom";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faSignInAlt, faUserPlus, faEnvelope } from '@fortawesome/free-solid-svg-icons';
+import { faSignInAlt, faUserPlus, faEnvelope, faBars, faTimes } from '@fortawesome/free-solid-svg-icons';
 import logo from '../assets/icons/logo.png'; // Adjust the path to your logo image
 
 const Navbar = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsOpen(!isOpen);
+  };
+
   return (
     <nav className="bg-gray-800 text-white p-4">
       <div className="container mx-auto flex justify-between items-center">
@@ -11,23 +18,48 @@ const Navbar = () => {
           <img src={logo} alt="Annamaya Vasundhara Logo" className="h-8 w-8 mr-1" />
           Annamaya Vasundhara
         </Link>
-        <div className="flex space-x-4">
-          <Link to="/" className="mx-2">Home</Link>
-          <Link to="/about" className="mx-2">About</Link>
-          <Link to="/dashboard" className="mx-2">Dashboard</Link>
-          <Link to="/login" className="mx-2 flex items-center">
+        <div className="hidden md:flex space-x-4">
+          <Link to="/" className="mx-2 hover:text-teal-200">Home</Link>
+          <Link to="/about" className="mx-2 hover:text-teal-200">About</Link>
+          <Link to="/dashboard" className="mx-2 hover:text-teal-200">Dashboard</Link>
+          <Link to="/login" className="mx-2 flex items-center hover:text-teal-200">
             <FontAwesomeIcon icon={faSignInAlt} className="mr-1" />
+            Login
           </Link>
-          <Link to="/signup" className="mx-2 flex items-center">
+          <Link to="/signup" className="mx-2 flex items-center hover:text-teal-200">
             <FontAwesomeIcon icon={faUserPlus} className="mr-1" />
             SignUp
           </Link>
-          <Link to="/contactus" className="mx-2 flex items-center">
+          <Link to="/contact" className="mx-2 flex items-center hover:text-teal-200">
             <FontAwesomeIcon icon={faEnvelope} className="mr-1" />
             Contact Us
           </Link>
         </div>
+        <div className="md:hidden flex items-center">
+          <button onClick={toggleMenu}>
+            <FontAwesomeIcon icon={isOpen ? faTimes : faBars} className="text-xl" />
+          </button>
+        </div>
       </div>
+      {isOpen && (
+        <div className="md:hidden bg-gray-800 text-white p-4">
+          <Link to="/" className="block mx-2 my-2 hover:text-teal-200" onClick={toggleMenu}>Home</Link>
+          <Link to="/about" className="block mx-2 my-2 hover:text-teal-200" onClick={toggleMenu}>About</Link>
+          <Link to="/dashboard" className="block mx-2 my-2 hover:text-teal-200" onClick={toggleMenu}>Dashboard</Link>
+          <Link to="/login" className="block mx-2 my-2 flex items-center hover:text-teal-200" onClick={toggleMenu}>
+            <FontAwesomeIcon icon={faSignInAlt} className="mr-1" />
+            Login
+          </Link>
+          <Link to="/signup" className="block mx-2 my-2 flex items-center hover:text-teal-200" onClick={toggleMenu}>
+            <FontAwesomeIcon icon={faUserPlus} className="mr-1" />
+            SignUp
+          </Link>
+          <Link to="/contact" className="block mx-2 my-2 flex items-center hover:text-teal-200" onClick={toggleMenu}>
+            <FontAwesomeIcon icon={faEnvelope} className="mr-1" />
+            Contact Us
+          </Link>
+        </div>
+      )}
     </nav>
   );
 };
